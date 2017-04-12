793b0c28ae4e0d94a954d3380e7041df89ea3ab9
Eric Desrochers <djaryzon@gmail.com>
2017-04-12 18:21:49 -0400

    Lab 11

--- target/generated-sources/annotations/com/soa/movieplex/entities/ShowTiming_.java
+++ target/generated-sources/annotations/com/soa/movieplex/entities/ShowTiming_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-12T17:41:10")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-12T18:12:24")
 @StaticMetamodel(ShowTiming.class)
 public class ShowTiming_ { 
 
